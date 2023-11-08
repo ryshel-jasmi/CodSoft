f39ff35e82feba352cdfe856f5865a40fbefcfdf
ryshel-jasmi <149150480+ryshel-jasmi@users.noreply.github.com>
2023-11-08 13:09:05 +0530

    Add files via upload

---
+++ Task 2/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+     });
+
+     $(window).on('scroll load',function(){
+
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
+     });
+
+     //smooth scrolling
+     $('a[href*="#').click(function(e){
+        e.preventDefault();
+        $('html, body').animated({
+            scrollTop : $($(this).attr('href')).offset().top,
+        },
+        500,
+        'linear'
+        );
+     });
+});
